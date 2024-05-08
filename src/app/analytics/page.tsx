b5e0ff82472bfe4fe8ecce7b9d2eaf2b7cfbd824
moshe <moshehadar001@gmail.com>
2024-05-08 14:51:32 +0300

    Small changes

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -30,34 +30,38 @@ const Controller = async () => {
     const topCountriesMap = new Map<string, number>()
 
     for (let i = 0; i < pageviews.length; i++) {
-      const day = pageviews[i];
-      if(!day) continue
+        const day = pageviews[i]
 
-      for (let j = 0; j < day.events.length; j++) {
-        const event = day.events[j];
-        if(!event) continue
+        if (!day) continue
 
-        const key = Object.keys(event)[0]!
-        const value = Object.values(event)[0]!
+        for (let j = 0; j < day.events.length; j++) {
+            const event = day.events[j]
 
-        const parsedKey = JSON.parse(key)
-        const country = parsedKey?.country
+            if (!event) continue
 
-        if (country) {
-          if (topCountriesMap.has(country)) {
-            const prevValue = topCountriesMap.get(country)!
-            topCountriesMap.set(country, prevValue + value)
-          } else {
-            topCountriesMap.set(country, value)
-          }
+            const key = Object.keys(event)[0]!
+            const value = Object.values(event)[0]!
+
+            const parsedKey = JSON.parse(key)
+            const country = parsedKey?.country
+
+            if (country) {
+                if (topCountriesMap.has(country)) {
+                    const prevValue = topCountriesMap.get(country)!
+                    topCountriesMap.set(country, prevValue + value)
+                } else {
+                    topCountriesMap.set(country, value)
+                }
+            }
         }
-      }
     }
 
-     const topCountries = [...topCountriesMap.entries()].sort((a, b) => {
-      if(a[1] > b[1]) return -1
-      else return 1
-     }).slice(0, 5)
+    const topCountries = [...topCountriesMap.entries()]
+        .sort((a, b) => {
+            if (a[1] > b[1]) return -1
+            else return 1
+        })
+        .slice(0, 5)
 
     return (
         <div className="min-h-screen w-full py-12 flex justify-center items-center">
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,10 @@ import { NextRequest, NextResponse } from "next/server"
 import { analytics } from "./utils/analytics"
 
 export default async function middleware(req: NextRequest) {
+    
     if (req.nextUrl.pathname === "/") {
         try {
-            analytics.track("pageview", {
+            await analytics.track("pageview", {
                 page: "/",
                 country: req.geo?.country,
             })
